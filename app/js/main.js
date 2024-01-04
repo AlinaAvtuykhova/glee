7f9d147948ff9058afe3e8d9715e4030786c09c7
AlinaAvtuykhova <aline_wine@vk.com>
2024-01-04 11:24:01 +0300

    added product-list

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,33 @@
-$(function() {
-  
-$('.slider__inner').slick({
-arrows: false,
-dots:true,
-fade:true
-});
+$(function () {
+
+  $('.slider__inner').slick({
+    arrows: false,
+    dots: true,
+    fade: true
+  });
+
+  $(".filter-price__input").ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
+$(".filter-recent__star").rateYo({
+  starWidth: "10px",
+  normalFill: "#d6d6d6",
+  ratedFill:"#ffcc00",
+  readOnly: true,
+  spacing: "5px",
+ });
 
 
-var mixer = mixitup('.products__items');
-var mixer = mixitup('.design__items');
+  var mixer = mixitup('.products__items');
+  var mixer = mixitup('.design__items');
 });
